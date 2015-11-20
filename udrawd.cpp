b1815c30dd58da4552e2ad4b312abc3e3d3f8274
Tim Sullivan <tsullivan@timatooth.com>
2015-11-20 23:17:55 +1300

    Updated readme and small code tidy up

--- udrawd.cpp
+++ udrawd.cpp
@@ -20,13 +20,13 @@ int main(int argc, char *argv[]) {
     boost::asio::ssl::context tls(boost::asio::ssl::context::sslv23);
     boost::system::error_code ec;
     std::string docroot;
-    
+
     if(argc > 3){
         tls.use_private_key_file(argv[1], boost::asio::ssl::context::pem);
         tls.use_certificate_chain_file(argv[2]);
         docroot = argv[3];
     } else {
-        std::cerr << "Usage: udrawd <server.key> <server.crt> <docroot>" << std::endl;
+        std::cerr << "Usage: udrawd <serverkey.pem> <servercert.pem> <docroot>" << std::endl;
         return -1;
     }
 
@@ -36,16 +36,17 @@ int main(int argc, char *argv[]) {
     redisContext *c;
     struct timeval timeout = {1, 500000}; // 1.5 seconds
     c = redisConnectWithTimeout("127.0.0.1", 6379, timeout);
-    
 
     server.handle("/canvases/main/", [c](const request &req, const response & res) {
-        //unsafe split, todo redo
         std::vector<std::string> strs;
         boost::split(strs, req.uri().path, boost::is_any_of("/"));
-        //danger zone
+        if(strs.size() != 6){
+            res.write_head(400);
+            res.end();
+            return;
+        }
         std::string key = strs[2] + ":" + strs[3] + ":" + strs[4] + ":" + strs[5];
-        
-        
+
         if(req.method() == "GET"){
             redisReply *reply;
             std::string query = "HGET tile:" + key + " data";
@@ -64,12 +65,12 @@ int main(int argc, char *argv[]) {
                 std::string response(reply->str, reply->len);
                 res.write_head(200, headers);
                 res.end(response);
-                
             } else {
                 res.write_head(500);
                 res.end();
                 std::cerr << "got a bad hiredis reply type #: " << reply->type << std::endl;
             }
+
             freeReplyObject(reply); //clean up redis response
 
         } else if (req.method() == "PUT") {
@@ -77,8 +78,6 @@ int main(int argc, char *argv[]) {
             std::shared_ptr<size_t> offset (new size_t(0)); //hold buffer size/position
             
             req.on_data([dataBox, offset, key, c, &res](const uint8_t *data, std::size_t len) {
-                //std::cout << "got data of size: " << len << std::endl;
-                //std::cout << "offset is:  " << *offset.get() << std::endl;
                 
                 if(len > 0){
                     if(*offset.get() + len > 300 * 1000 - 1){
@@ -91,8 +90,7 @@ int main(int argc, char *argv[]) {
                     //copy the incoming data into buffer at offset levels
                     memcpy(dataBox + *offset.get(), data, len);
                     *offset.get() += len;
-                } else if(len == 0){
-                    //write into redis
+                } else if (len == 0) { //got EOF
 
                     redisReply *insertTile = (redisReply*) redisCommand(c, "HSET tile:%s data %b", key.c_str(), dataBox, *offset.get());
                     if(insertTile->type == REDIS_REPLY_INTEGER){
@@ -104,15 +102,13 @@ int main(int argc, char *argv[]) {
                         res.end();
                         std::cerr << "Unexpected save reply status is #" << insertTile->type << std::endl;
                     }
-                    
+
                     //cleanup time
                     freeReplyObject(insertTile);
                     free(dataBox);
-                    
                 }
             });
         }
-        
     });
     
     server.handle("/socket.io/", [](const request &req, const response & res) {
@@ -121,14 +117,13 @@ int main(int argc, char *argv[]) {
         res.write_head(301, header);
         res.end();
     });
-    
-    
+
     server.handle("/", [&docroot](const request &req, const response & res) {
         res.write_head(200);
         std::cout << "serving: " << docroot << "/static/index.html" << std::endl;
         res.end(file_generator(docroot + "/static/index.html"));
     });
-    
+
     server.handle("/static/", [&docroot](const request &req, const response & res) {
         auto path = percent_decode(req.uri().path);
         if (!check_path(path)) {
@@ -136,16 +131,15 @@ int main(int argc, char *argv[]) {
           res.end();
           return;
         }
-       
+
         path = docroot + path;
-        //std::cout << "opening " << path << std::endl;
         auto fd = open(path.c_str(), O_RDONLY);
         if (fd == -1) {
           res.write_head(404);
           res.end();
           return;
         }
-        
+
         auto header = header_map();
         struct stat stbuf;
         if (stat(path.c_str(), &stbuf) == 0) {
@@ -156,11 +150,11 @@ int main(int argc, char *argv[]) {
         }
         res.write_head(200, std::move(header));
         res.end(file_generator_from_fd(fd));
-        
+
     });
-    
+
     std::cerr << "Listening on 0.0.0.0:3000" << std::endl;
-    
+
     if (server.listen_and_serve(ec, tls, "0.0.0.0", "3000")) {
         std::cerr << "error: " << ec.message() << std::endl;
     }
